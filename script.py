d419e469345f4f90bc9dee3bac7fcba58c4a6027
henrique <henriquesevdacunha@gmail.com>
2024-06-18 10:14:10 -0400

    ta certo

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# Load the corrected CSV file with comma as the delimiter
-file_path_corrected = 'focos_br_ref_2004.csv'
-df = pd.read_csv(file_path_corrected, delimiter=',')
+# Carregar o arquivo CSV corrigido com vírgula como delimitador
+caminho_arquivo_corrigido = 'focos_br_ref_2004.csv'
+df = pd.read_csv(caminho_arquivo_corrigido, delimiter=',')
 
-# Save the dataframe to an Excel file
-output_excel_file = 'focos_br_ref_2004_corrected.xlsx'
-df.to_excel(output_excel_file, index=False, engine='openpyxl')
+# Salvar o dataframe em um arquivo Excel
+arquivo_excel_saida = 'focos_br_ref_2004_corrigido.xlsx'
+df.to_excel(arquivo_excel_saida, index=False, engine='openpyxl')
 
-print(f"CSV file has been fixed and saved as {output_excel_file}")
+print(f"O arquivo CSV foi corrigido e salvo como {arquivo_excel_saida}")
